bbcd4d203c2e6af0c9672396eb9d1af9aa02d43f
Saulius Sirvydas <sirvydas123s@gmail.com>
2023-08-28 21:41:50 +0300

    table: add component skeleton

---
+++ src/components/table/MTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="(column, index) in columns" :key="index"></th>
+      </tr>
+    </thead>
+  </table>
+</template>
+
+<script setup lang="ts">
+interface TableColumn {
+  title: string
+  prop: string
+}
+
+interface Props {
+  columns: TableColumn[]
+  hasMinimap?: boolean
+  rowSize?: 'sm' | 'lg'
+  hasColumnResize?: boolean
+}
+
+defineProps<Props>()
+</script>
