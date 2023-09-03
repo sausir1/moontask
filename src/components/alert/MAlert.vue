66bf8e5d101853988338fa1c2769d9dea83d2a41
Saulius Sirvydas <saulius.sirvydas@teltonika.lt>
2023-09-03 15:07:47 +0300

    start implementing m-alert

---
+++ src/components/alert/MAlert.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="m-alert p-4 rounded-ds-s-sm" role="alert">
+    <div class="flex">
+      <header class="flex grow">
+        <slot name="leading"></slot>
+        <h3 class="alert-title grow text-ds-bulma font-semibold">
+          <slot></slot>
+        </h3>
+      </header>
+      <section class="alert-content text-bulma mt-2">
+        <slot></slot>
+      </section>
+      <div class="shrink-0">
+        <slot name="trailing"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// TODO think about how to solve the dynamic style
+interface Props {
+  border: boolean
+  borderColor: string
+}
+defineProps()
+</script>
