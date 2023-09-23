d83b6a2116faa69b2624c193a2a19e9143a059d9
Saulius Sirvydas <sirvydas123s@gmail.com>
2023-09-23 16:52:40 +0300

    implement checkbox

--- src/components/checkbox/MCheckbox.vue
+++ src/components/checkbox/MCheckbox.vue
@@ -1,35 +1,48 @@
 <template>
-  <label
-    class="flex min-w-0 truncate text-bulma items-center gap-2 cursor-pointer text-14-ds"
-    :class="{
-      'cursor-not-allowed': disabled,
-      'opacity-60 pointer-events-none': readonly,
-    }"
-  >
-    <input
-      class="w-4 h-4 p-1 rounded-ds-i-xs overflow-hidden focus:ring-0 focus:outline-0 focus:ring-offset-0 focus:shadow-none focus:outline-offset-0 border-ds-trunks text-ds-piccolo transition-colors"
-      :class="{ 'cursor-not-allowed': disabled }"
-      type="checkbox"
-      :disabled="disabled"
-      :readonly="props.readonly"
-      :indeterminate="indeterminate"
-    />
-    <span class="select-none"><slot>Select this for big money</slot></span>
+  <label>
+    <!-- // :id="$attrs?.id as string" -->
+    <div class="relative flex text-14-ds items-center gap-2 cursor-pointer">
+      <input
+        v-model="checked"
+        class="w-6 h-6 appearance-none peer select-none"
+        :class="{ 'cursor-not-allowed': disabled }"
+        type="checkbox"
+        :disabled="disabled"
+        :readonly="props.readonly"
+        :indeterminate="indeterminate"
+        :checked="indeterminate ? false : checked"
+        :aria-checked="indeterminate ? 'mixed' : `${checked}`"
+      />
+      <span
+        class="inline-block transition-all absolute top-1 left-1 h-4 w-4 outline outline-[1.5px] rounded-ds-i-xs outline-ds-trunks -outline-offset-1 peer-aria-checked:outline-none peer-indeterminate:outline-none"
+        :class="{ [checkedColor]: checked || indeterminate }"
+      ></span>
+      <MIcon
+        :icon="indeterminate ? 'Minus' : 'Check'"
+        class="absolute top-1/2 left-1 -translate-y-1/2 !w-4 !h-4 stroke-2 peer-indeterminate:opacity-100 peer-indeterminate:scale-100 peer-aria-checked:opacity-100 peer-aria-checked:scale-100 scale-50 opacity-0 transition-all text-ds-goten"
+        aria-hidden="true"
+      ></MIcon>
+      <span><slot /> {{ indeterminate }}-{{ checked }}</span>
+    </div>
   </label>
 </template>
 
 <script setup lang="ts">
 interface Props {
+  variant?: 'primary' | 'outline'
+  checkedColor?: string
   indeterminate?: boolean
-  checked?: boolean
   disabled?: boolean
   readonly?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  indeterminate: true,
-  checked: false,
+  variant: 'primary',
+  checkedColor: 'bg-ds-piccolo',
+  indeterminate: false,
   disabled: false,
   readonly: false,
 })
+
+const checked = defineModel({ required: false, default: false })
 </script>
--- src/views/components/inputs/CheckBox.vue
+++ src/views/components/inputs/CheckBox.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="grid grid-cols-[repeat(auto-fill,minmax(14rem,1fr))] gap-4">
-    <MCheckbox></MCheckbox>
-    <MCheckbox :readonly="true">Read only</MCheckbox>
-    <MCheckbox :disabled="true">Disabled</MCheckbox>
-    <MCheckbox indeterminate>indeterminate</MCheckbox>
+    <MCheckbox checked-color="bg-ds-krillin" v-model="value"></MCheckbox>
+    <MCheckbox v-model="value" :readonly="true">Read only</MCheckbox>
+    <MCheckbox v-model="value" :disabled="true">Disabled</MCheckbox>
+    <MCheckbox v-model="value" indeterminate>indeterminate</MCheckbox>
+  </div>
+  <div class="my-12">
+    <h4 class="text-32-ds font-semibold">TEST</h4>
+    <MCheckbox :indeterminate="isIndetermined" v-model="allSelected"
+      >Select All</MCheckbox
+    >
+    <MCheckbox v-model="selects[0]">Select 1</MCheckbox>
+    <MCheckbox v-model="selects[1]">Select 2</MCheckbox>
+    <MCheckbox v-model="selects[2]">Select 3</MCheckbox>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const value = ref(false)
+const selects = ref([false, false, false])
+const isIndetermined = computed(() => {
+  return selects.value.includes(true) && selects.value.includes(false)
+})
+const allSelected = computed({
+  get() {
+    return !selects.value.includes(false)
+  },
+  set(v) {
+    if (v) selects.value.forEach((_, index) => (selects.value[index] = true))
+    else selects.value = selects.value.map((v) => false)
+  },
+})
+</script>
 
 <style scoped></style>
