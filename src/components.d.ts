1aaa2f5137e28a18950db4834cb0d173c47717b2
sausir1 <sirvydas123s@gmail.com>
2023-10-15 19:08:40 +0300

    remove comments

--- src/components.d.ts
+++ src/components.d.ts
@@ -1,7 +1,5 @@
 import MIcon from '@/components/icons/MIcon.vue'
 declare module '@vue/runtime-core' {
-  // Vue >= 2.7
-  // declare module '@vue/runtime-dom' {  // Vue <= 2.6.14
   export interface GlobalComponents {
     MIcon: typeof MIcon
   }
